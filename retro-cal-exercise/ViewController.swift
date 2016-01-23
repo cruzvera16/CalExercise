3f57798fbfd9475a7be641883af6570534e4ec07
cruzvera16 <cmvera16@gmail.com>
2016-01-23 15:05:21 -0500

    made all functions work

--- retro-cal-exercise/ViewController.swift
+++ retro-cal-exercise/ViewController.swift
@@ -12,10 +12,22 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var outputDisplay: UILabel!
     
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Empty = "Empty"
+        case Equal = "="
+        case Perrcentaje = "%"
+        case Clear = "C"
+        case AllClear = "AC"
+    }
+    
     var runningNumber = ""
     var leftValStr = ""
     var rightValStr = ""
-    var currentOperation = ""
+    var currentOperation: Operation = Operation.Empty
     var currentSign = ""
     var result = ""
     var dotPresent = false
@@ -36,14 +48,94 @@ class ViewController: UIViewController {
     }
     
     @IBAction func signPressed(sign: UIButton) {
-        if currentSign == "" {
-            currentSign = "-"
-        } else {
-            currentSign = ""
-        }
-    
-        outputDisplay.text = "\(currentSign) \(runningNumber)"
+        runningNumber = outputDisplay.text!
+        runningNumber = "\(-Double(runningNumber)!)"
+        outputDisplay.text = runningNumber
         
+        
+//        if currentSign == "" {
+//            currentSign = "-"
+//        } else {
+//            currentSign = ""
+//        }
+//    
+//        outputDisplay.text = "\(currentSign) \(runningNumber)"
+        
+    }
+    
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        processOperation(Operation.Multiply)
+    }
+    
+    @IBAction func onDividePressed(sender: AnyObject) {
+        processOperation(Operation.Divide)
+    }
+    
+
+    @IBAction func onSubtractPressed(sender: AnyObject) {
+        processOperation(Operation.Subtract)
+    }
+    
+    @IBAction func onAddPressed(sender: AnyObject) {
+        processOperation(Operation.Add)
+    }
+    
+    @IBAction func onEqualPressed(sender: AnyObject) {
+        processOperation(Operation.Equal)
+    }
+    
+    @IBAction func onPercentajePressed(sender: AnyObject) {
+        runningNumber = "\(Double(runningNumber)! / 100)"
+        outputDisplay.text = runningNumber
+    }
+    
+    @IBAction func onClearPressed(sender: AnyObject) {
+        runningNumber = ""
+        outputDisplay.text = runningNumber
+    }
+    
+    @IBAction func onAllClearPressed(sender: AnyObject) {
+        runningNumber = ""
+        outputDisplay.text = runningNumber
+        leftValStr = ""
+        rightValStr = ""
+        currentOperation = Operation.Empty
+        currentSign = ""
+        dotPresent = false
+    }
+    
+    func processOperation(op: Operation) {
+        if currentOperation != Operation.Empty {
+            if runningNumber != "" {
+                
+                rightValStr = outputDisplay.text!
+                runningNumber = ""
+                currentSign = ""
+                
+                if currentOperation == Operation.Multiply {
+                    result = "\(Double(leftValStr)! * Double(rightValStr)!)"
+                }
+                if currentOperation == Operation.Divide {
+                    result = "\(Double(leftValStr)! / Double(rightValStr)!)"
+                }
+                if currentOperation == Operation.Subtract {
+                    result = "\(Double(leftValStr)! - Double(rightValStr)!)"
+                }
+                if currentOperation == Operation.Add {
+                    result = "\(Double(leftValStr)! + Double(rightValStr)!)"
+                }
+                leftValStr = result
+                outputDisplay.text = result
+            }
+            currentOperation = op
+            
+        } else { if runningNumber != "" {
+            leftValStr = outputDisplay.text!
+            runningNumber = ""
+            currentOperation = op
+            currentSign = ""
+            }
+        }
     }
 
     override func viewDidLoad() {
